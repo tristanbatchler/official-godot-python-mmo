3bc19ba91f8a35cc4af5707279ac2e16df206103
Tristan Batchler <tristan@careerhub.com.au>
2022-12-01 20:06:34 +1000

    Correct where to set self._state

--- server/protocol.py
+++ server/protocol.py
@@ -7,7 +7,7 @@ class GameServerProtocol(WebSocketServerProtocol):
     def __init__(self):
         super().__init__()
         self._packet_queue: queue.Queue[tuple['GameServerProtocol', packet.Packet]] = queue.Queue()
-        self._state: callable = None
+        self._state: callable = self.PLAY
 
     def PLAY(self, sender: 'GameServerProtocol', p: packet.Packet):
         if p.action == packet.Action.Chat:
@@ -35,7 +35,6 @@ class GameServerProtocol(WebSocketServerProtocol):
     # Override
     def onOpen(self):
         print(f"Websocket connection open.")
-        self._state = self.PLAY
 
     # Override
     def onClose(self, wasClean, code, reason):
