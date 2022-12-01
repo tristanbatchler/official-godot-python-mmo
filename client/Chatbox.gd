ec7cf14f16354247cddef519a091c43063886ff9
Tristan Batchler <tristan@careerhub.com.au>
2022-12-02 06:50:49 +1000

    Allow pressing a button to chat (support for mobile devices)

--- client/Chatbox.gd
+++ client/Chatbox.gd
@@ -3,12 +3,14 @@ extends Control
 onready var chat_log = get_node("CanvasLayer/VBoxContainer/RichTextLabel")
 onready var input_label = get_node("CanvasLayer/VBoxContainer/HBoxContainer/Label")
 onready var input_field = get_node("CanvasLayer/VBoxContainer/HBoxContainer/LineEdit")
+onready var button = get_node("CanvasLayer/VBoxContainer/HBoxContainer/Button")
 
 signal message_sent(message)
 
 
 func _ready():
 	input_field.connect("text_entered", self, "text_entered")
+	button.connect("pressed", self, "button_pressed")
 
 
 func _input(event: InputEvent):
@@ -33,3 +35,6 @@ func text_entered(text: String):
 		input_field.text = ""
 
 		emit_signal("message_sent", text)
+
+func button_pressed():
+	text_entered(input_field.text)
