d6947cb169ab68adb9f2530f5da71366a41b92b0
Tristan Batchler <tristan@careerhub.com.au>
2022-12-20 06:01:26 +1000

    Update client domain

--- client/Main.gd
+++ client/Main.gd
@@ -21,7 +21,7 @@ func _ready():
 	_network_client.connect("error", self, "_handle_network_error")
 	_network_client.connect("data", self, "_handle_network_data")
 	add_child(_network_client)
-	_network_client.connect_to_server("127.0.0.1", 8081)
+	_network_client.connect_to_server("godmmo.tx2600.net", 8081)
 	
 	_login_screen.connect("login", self, "_handle_login_button")
 	_login_screen.connect("register", self, "_handle_register_button")
